476f54aed7c9042ea31affc1d3444c0d911673d5
Harim_Chung <harimchung1998@gmail.com>
2022-08-01 23:07:20 +0900

    0801 BOJ

---
+++ BOJ/1269.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split())
+set_a = set(map(int, input().split()))
+set_b = set(map(int, input().split()))
+print(len(set_a ^ set_b))
--- BOJ/2884.py
+++ BOJ/2884.py
@@ -1,10 +1,34 @@
-a, b = map(int, input().split())
+# a, b = map(int, input().split())
 
-if b >= 45:
-    print(a, b-45)
+# if b >= 45:
+#     print(a, b-45)
 
-else:
-    if a == 0:
-        print(23, b+15)
-    else:
-        print(a-1, b + 15)
+# else:
+#     if a == 0:
+#         print(23, b+15)
+#     else:
+#         print(a-1, b + 15)
+
+# numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# for i in range(len(numbers)):
+#     for j in range(len(numbers)):
+#         print(numbers[j][i], end=' ')
+
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def walk(self):
+#         print('걷는다!')
+
+#     def eat(self):
+#         print(f'{self.name}!먹는다!')
+
+# dog = Animal('dog')
+# dog.walk()
+
+lunch = ['짜장면', '짬뽕', '탕수육']
+
+for idx, num in enumerate(lunch):
+    print(idx, num)
