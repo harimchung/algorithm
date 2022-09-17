2768dfa4e27bdf4a7ef47b2db2c23524f8601c4f
Harim_Chung <109330637+harimchung@users.noreply.github.com>
2022-09-17 23:02:07 +0900

    0917 드디어 풀어부렀다구!!!

--- BOJ/class_3/16236_아기상어2.py
+++ BOJ/class_3/16236_아기상어2.py
@@ -1,19 +1,12 @@
 import sys
 from collections import deque
 
-# 왜 empty que에서 빼는 현상이 생기는 건지?
-# 높이가 같을 때, 왼쪽에서 먼저 빼기 위해서 어떻게 처리해야 할지?
-
-dx = [-1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def babyshark (i, j):
     # i, j는 시작하는 정점
-    global que
-    global time
-    global arr
-    global shark_size
-    global eat
+    global que, time, arr, shark_size, eat
 
     visit_cnt = 1
     visited = [[-1]*n for _ in range(n)]
@@ -31,6 +24,7 @@ def babyshark (i, j):
                     if 0 < arr[ni][nj] < shark_size:
                         eat_list.append((ni, nj))
         visit_cnt += 1
+
         # 다음으로 이동할 곳 탐색 (먹을 생선)
         if eat_list:
             eat += 1
@@ -41,8 +35,8 @@ def babyshark (i, j):
             ei, ej = min(eat_list) # 다음으로 이동할 곳 선정
             que.clear()
             que.append((ei, ej)) # 이동할 곳 큐에 넣기
-            arr[ei][ej] = 0 # 먹음처리
-            time += arr[ei][ej] # 이동하는데 걸린 시간 추가
+            time += visited[ei][ej] # 이동하는데 걸린 시간 추가
+            arr[ei][ej] = 0 # 먹었다고 처리
             babyshark(ei, ej) # 새로운 곳에서 재실행
 
 
