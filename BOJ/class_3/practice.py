54a460d040df4d3670d0dd06b0abd240a55488c2
harimchung <harimchung1998@gmail.com>
2022-09-23 18:00:53 +0900

    0923

--- BOJ/class_3/practice.py
+++ BOJ/class_3/practice.py
@@ -13,4 +13,23 @@
 # else:
 #     print("no")
 
-print(3<5,6<7)
+import sys
+# n은 회의의 수
+n = int(sys.stdin.readline())
+meetings = []
+for _ in range(n):
+    s, e = map(int, sys.stdin.readline().split())
+    meetings.append([s,e])
+select = []
+
+s_m = sorted(meetings, key=lambda id:id[1])
+
+for i in range(n):
+    if not select:
+        select.append(s_m[i])
+        continue
+    end = select[-1][1]
+    if s_m[i][0] >= end:
+        select.append(s_m[i])
+
+print(len(select))
